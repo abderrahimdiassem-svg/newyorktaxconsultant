4f7ea9662bcc6160bc82b86ebb1b77880fa60e00
Abderrahim <abderrahimdiassem@gmail.com>
2026-05-21 17:53:22 +0100

    Fixed IndexNow keyLocation

--- app/api/indexnow/route.js
+++ app/api/indexnow/route.js
@@ -10,7 +10,7 @@ export async function POST(req) {
         host: "newyorktaxconsultant.com",
         key: "11e79b40ba0841b59846b7ebd3029e55",
         keyLocation:
-          "https://yourdomain.com/11e79b40ba0841b59846b7ebd3029e55.txt",
+          "https://newyorktaxconsultant.com/11e79b40ba0841b59846b7ebd3029e55.txt",
         urlList: urls,
       }),
     });
