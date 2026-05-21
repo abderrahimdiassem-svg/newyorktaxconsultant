2915457b49fb70187ebae67dab68765eae895a9d
Abderrahim <abderrahimdiassem@gmail.com>
2026-05-21 17:50:03 +0100

    Added IndexNow API

---
+++ app/api/indexnow/route.js
@@ -0,0 +1,21 @@
+export async function POST(req) {
+    const urls = await req.json();
+  
+    const response = await fetch("https://api.indexnow.org/IndexNow", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        host: "newyorktaxconsultant.com",
+        key: "11e79b40ba0841b59846b7ebd3029e55",
+        keyLocation:
+          "https://yourdomain.com/11e79b40ba0841b59846b7ebd3029e55.txt",
+        urlList: urls,
+      }),
+    });
+  
+    return new Response(await response.text(), {
+      status: response.status,
+    });
+  }
